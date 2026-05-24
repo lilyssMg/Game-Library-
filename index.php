a269567215b143f245e506e8babf52b2a32a8c45
Michy <mich.davila08@gmail.com>
2026-05-24 15:55:18 +0800

    Create index.php for Game Library project
    Add initial PHP files for Game Library project

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php // Entry point ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Game Library</title>
+</head>
+<body>
+
+<h1>Game Library</h1>
+
+<p>Welcome to our Game Library project.</p>
+
+<ul>
+    <li><a href="games.php">Browse Games</a></li>
+    <li><a href="add_game.php">Add Game</a></li>
+    <li><a href="member.php">Group Members</a></li>
+</ul>
+
+</body>
+</html>
