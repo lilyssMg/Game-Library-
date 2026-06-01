60d952302f42fba38897a5ec7ba0663e252e5232
yet6987-arch <yet6987@gmail.com>
2026-06-01 22:20:46 +0800

    Style games page
    Updated the display of games to use a card layout for better presentation.

--- games.php
+++ games.php
@@ -22,31 +22,54 @@ while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
 <html>
 <head>
     <title>Games</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<h1>Game List</h1>
+<nav>
+    <a href="index.php">Home</a>
+    <a href="games.php">Games</a>
+    <a href="add_game.php">Add Game</a>
+    <a href="member.php">Members</a>
+</nav>
 
-<a href="index.php">Home</a>
+<h1>Game List</h1>
 
 <hr>
 
 <h2>predefined games</h2>
 
 <?php foreach ($games as $game): ?>
-    <p>
-        <strong><?php echo htmlspecialchars($game[0]); ?></strong>
-        (<?php echo htmlspecialchars($game[1]); ?>)
-    </p>
+    <div class="card">
+
+    <strong>
+        <?php echo htmlspecialchars($game[0]); ?>
+    </strong>
+
+    <br>
+
+    Genre:
+    <?php echo htmlspecialchars($game[1]); ?>
+
+</div>
 <?php endforeach; ?>
 
 <h2>User defined games</h2>
 
 <?php foreach ($user_defined_games as $game): ?>
-    <p>
-        <strong><?php echo htmlspecialchars($game[0]); ?></strong>
-        (<?php echo htmlspecialchars($game[1]); ?>)
-    </p>
+    
+    <div class="card">
+
+    <strong>
+        <?php echo htmlspecialchars($game[0]); ?>
+    </strong>
+
+    <br>
+
+    Genre:
+    <?php echo htmlspecialchars($game[1]); ?>
+
+</div>
 <?php endforeach; ?>
 </body>
 </html>
